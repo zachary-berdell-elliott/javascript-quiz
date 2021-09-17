6b3f15cf91fa89f98e616909aeef6549b58f302c
zachary <zacharyberdell@gmail.com>
2021-09-17 14:08:20 -0600

    Added a layout of some functions that can be used. Some work done on the html

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+var startGameBtn = document.querySelector("#game-starter");
+var qAndAs = {
+    
+}
+
+//Starts the game when button is clicked
+startGameBtn.addEventListener("click", runGame);
+
+//function for running the game
+function runGame() {
+
+}
+
+//function for operating the timer
+function timer() {
+
+}
+
+function questionPicker()
