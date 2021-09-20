f5f05ac0023791754fbb01f0b30c11c0217ee1c6
Zachary Elliott <zacharyberdell@gmail.com>
2021-09-20 14:08:48 -0600

    Working on the question display popping up. Fixed syntax with some variables and a loop

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,24 @@
 var startGameBtn = document.querySelector("#starter");
 var startScreen = document.querySelector("#start-screen");
-const qAndAs = {
-    questions: ["How do you change the text of an html element in Javascript?", "Which of these is not a valid declaration type?", "What tag is used to import Javascript into an html document?", "How do you save a value to the local storage of a browser?", "What loop starts with a variable declaration, then a condition to run on, then an iteration advancement."],
+const qAndAs = [
+    {
+    question: "How do you change the text of an html element in Javascript?", 
+    options: ["innerContent", "textContent", "text", "innerText"],
+    answer: "textContent",
+    }
+]
+   /* questions: ["How do you change the text of an html element in Javascript?", "Which of these is not a valid declaration type?", "What tag is used to import Javascript into an html document?", "How do you save a value to the local storage of a browser?", "What loop starts with a variable declaration, then a condition to run on, then an iteration advancement."],
     firstAnswers: ["innerContent", "textContent", "text", "innerText"],
     secondAnswers: ["var", "int", "const", "let"],
     thirdAnswers: ["code", "import", "script", "link"],
     fourthAnswers: ["localContent", "localCache", "sessionStorage", "localStorage"],
     fifthAnswers: ["for", "while", "for each", "do while"],
-}
+]*/
 var timeDisplay = document.querySelector("#timer");
 var questionDiv = document.querySelector("#question-div");
-
-
-
+var questionOptions = document.querySelector("#options");
+var questionIndex = 0;
+var questionTitle = document.querySelector("#question-title");
 
 
 var timeStart = 40;
@@ -38,14 +44,17 @@ function startGame() {
     console.log("start game working");
     timer();
     questionDiv.removeAttribute("class");
+    questionPicker();
 }
 //function for running the questions
-function questionPicker(_question, _answerArray, _correctAnswer) {
+function questionPicker() {
+    var displayedQuestion = qAndAs[questionIndex];
+    questionTitle.textContent = displayedQuestion.question;
     //Adds the question to the top of the page
-    displayedQuestion = document.querySelector("#question-title").textContent = _question.value;
+    questionTitle.textContent = _question.value;
     questionForm = document.createElement("form");
 
-    for(let i = _answerArray.length-1; i >= 0; i--){
+    for(let i = 0; i >= qAndAs.options.length-1; i++){
         document.createElement()
     }
 
