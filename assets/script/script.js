ed2eb751632fe1fdcb8a2ca83e35f95f96ab49d7
zachary <zacharyberdell@gmail.com>
2021-09-20 10:07:57 -0600

    Plans added for the layout of functions. Work done on the questionPicker function.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,19 +1,40 @@
 var startGameBtn = document.querySelector("#game-starter");
-var qAndAs = {
-    
+const qAndAs = {
+    questions = ["How do you change the text of an html element in Javascript?", "Which of these is not a valid declaration type?", "What tag is used to import Javascript into an html document?", "How do you save a value to the local storage of a browser?", "What loop starts with a variable declaration, then a condition to run on, then an iteration advancement."],
+    firstAnswers= ["innerContent", "textContent", "text", "innerText"],
+    secondAnswers = ["var", "int", "const", "let"],
+    thirdAnswers = ["code", "import", "script", "link"],
+    fourthAnswers = ["localContent", "localCache", "sessionStorage", "localStorage"],
+    fifthAnswers = ["for", "while", "for each", "do while"],
 }
+const timeStart = 40;
 
 //Starts the game when button is clicked
-startGameBtn.addEventListener("click", runGame);
-
-//function for running the game
-function runGame() {
-
-}
+startGameBtn.addEventListener("click", function () {
+   // timer();
+    questionPicker(qAndAs.questions[0], qAndAs.firstAnswers, qAndAs.firstAnswers[1]);
+});
 
 //function for operating the timer
 function timer() {
-
+    setInterval(timeStart * 1000);
 }
 
-function questionPicker()
+//function for running the questions
+function questionPicker(_question, _answerArray, _correctAnswer) {
+    //Adds the question to the top of the page
+    displayedQuestion = document.createElement("h2").textContent = _question.value;
+    questionForm = document.createElement("form");
+
+    for(let i = _answerArray.length-1; i >= 0; i--){
+        document.createElement()
+    }
+
+    //deducts time if the user answers the question incorrectly
+    if (userSelection !== _correctAnswer){
+        timeLeft -= 20;
+    }
+
+
+    //Removes the elements displayed to continue to the next question
+}
